86cf0e7b6d7878133ba543e520253b35d31a2696
hardik0812 <mail2hardikchandegara@gmail.com>
2021-05-20 14:36:10 +0530

    speed

---
+++ speed.js
@@ -0,0 +1,18 @@
+
+checkSpeed(13);
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint) {
+    console.log ('Ok');
+    return; 
+  }
+
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= 12)
+    console.log('suspended');
+  else
+    console.log('Points', points);
+}
