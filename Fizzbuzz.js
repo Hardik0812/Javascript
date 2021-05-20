fcce8326d0bc40cb556ae4b99972a21bb6059261
hardik0812 <mail2hardikchandegara@gmail.com>
2021-05-20 14:37:36 +0530

    FizzbuZZ

---
+++ Fizzbuzz.js
@@ -0,0 +1,18 @@
+const output = fizzbuzz(3);
+console.log(output);
+
+
+function fizzbuzz(input) {
+
+    if((input % 3 === 0) && (input % 5 === 0))
+    return 'Fizzbuzz';
+
+    if (input % 3 === 0) 
+    return "Fizz";
+
+    if (input % 5 === 0)
+    return "Buzz";
+    
+    if (typeof input !== 'number')
+    return NaN;
+}
